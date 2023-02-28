47122b7d5a24b61ed07da1e507c4d3b7986ec553
dev-kev-coder <recodedkevdev@gmail.com>
2023-02-28 09:48:15 -0800

    imported in DeploymentTools components

--- src/js/App.js
+++ src/js/App.js
@@ -1,13 +1,17 @@
+// React
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// Components
+import DeploymentTools from "./pages/DeploymentTools/DeploymentTools";
+
+// Css
 import "./index.css";
+
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <button onClick={() => electron.notificationApi.sendNotifications("test")}>
-      Notify
-    </button>
+    <DeploymentTools />
   </React.StrictMode>
 );
